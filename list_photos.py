527ea1aa4991445757c27164aa9be723fb6164a8
Clyde <clydemonroe3@gmail.com>
2023-10-12 16:14:44 -0400

    Added Detailed Comments to Core Files
    Enhanced the clarity and maintainability of the project by adding comprehensive comments to index.html, list_photos.py, and script.js. These comments detail the thought process behind each code section.

--- list_photos.py
+++ list_photos.py
@@ -1,9 +1,22 @@
+# Importing the boto3 library, which is the AWS SDK for Python.
+# This allows for making API requests to AWS directly from Python.
 import boto3
 
+# The main function that AWS Lambda will trigger.
 def handler(event, context):
+    # Initializing the S3 client using boto3.
+    # This client provides a way to interact with the AWS S3 service.
     s3 = boto3.client('s3')
+
+    # Listing objects (in this case, photos) from the specified S3 bucket.
     objects = s3.list_objects(Bucket='clyde-serverless-photo-album-2023')
+
+    # Using a list comprehension to extract just the 'Key' of each object.
+    # The 'Key' typically refers to the file name or the path of the file in the bucket.
     photos = [obj['Key'] for obj in objects.get('Contents', [])]
+
+    # Returning a successful HTTP response with a status code of 200.
+    # The body of the response contains the list of photo keys.
     return {
         'statusCode': 200,
         'body': photos
--- script.js
+++ script.js
@@ -1,21 +1,34 @@
 function fetchPhotos() {
-    // Replace this with your API Gateway URL
+    // I've set my API Gateway endpoint to the 'apiUrl' constant.
     const apiUrl = "https://tqocxfa5ui.execute-api.us-east-1.amazonaws.com/prod/photos";
 
+    // I'm making an HTTP request to the API I set up using the 'fetch' method.
     fetch(apiUrl)
-        .then(response => response.json())
+        .then(response => {
+            // Here, I'm converting the response I received into a JSON format.
+            return response.json();
+        })
         .then(data => {
+            // I'm selecting the 'photoList' element from the HTML to display the photos.
             const photoList = document.getElementById('photoList');
-            photoList.innerHTML = ''; // Clear any existing photos
 
-            // Loop through each photo and add to the list
+            // I'm clearing out any existing content to ensure the new list doesn't overlap with any old data.
+            photoList.innerHTML = '';
+
+            // I'm looping through each photo I got from the response.
             data.body.forEach(photo => {
+                // For every photo, I'm creating a new list item.
                 const listItem = document.createElement('li');
+
+                // I'm setting the content of the list item to the name of the photo.
                 listItem.textContent = photo;
+
+                // Then, I'm appending this list item to the 'photoList' so it's visible on the website.
                 photoList.appendChild(listItem);
             });
         })
         .catch(error => {
+            // If anything goes wrong, I'll catch the error here and log it for my reference.
             console.error('There was an error fetching the photos:', error);
         });
 }
